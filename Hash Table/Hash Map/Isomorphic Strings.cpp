a0714a6ddac3c4a453f18d880da060fcd9628ee8
Kshitij Dwivedi <kshitijdwivedi28@gmail.com>
2022-04-03 17:02:51 +0530

    Create Isomorphic Strings.cpp

---
+++ Hash Table/Hash Map/Isomorphic Strings.cpp
@@ -0,0 +1,54 @@
+//  Isomorphic Strings
+
+// Solution
+// Given two strings s and t, determine if they are isomorphic.
+
+// Two strings s and t are isomorphic if the characters in s can be replaced to get t.
+
+// All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character, but a character may map to itself.
+
+ 
+
+// Example 1:
+
+// Input: s = "egg", t = "add"
+// Output: true
+// Example 2:
+
+// Input: s = "foo", t = "bar"
+// Output: false
+// Example 3:
+
+// Input: s = "paper", t = "title"
+// Output: true
+ 
+
+// Constraints:
+
+// 1 <= s.length <= 5 * 104
+// t.length == s.length
+// s and t consist of any valid ascii character.
+
+
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        
+        unordered_map<int, int> ms;
+        unordered_map<int, int> mt;
+        
+        for(int i = 0; i < s.size(); i++)
+        {
+            if (ms[s[i]] != mt[t[i]])
+                return false;
+            
+            ms[s[i]] = i+1;
+            mt[t[i]] = i+1;
+        }
+        
+        return true;
+        
+        
+        
+    }
+};
