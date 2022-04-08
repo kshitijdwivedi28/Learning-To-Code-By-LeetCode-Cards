c6e3d57767e01bf1f570e7bdc853bc5d384f789c
Kshitij Dwivedi <kshitijdwivedi28@gmail.com>
2022-04-09 02:55:47 +0530

    Create Remove Nth Node from end of List.cpp

---
+++ Linked List/Two Pointer Technique/Remove Nth Node from end of List.cpp
@@ -0,0 +1,92 @@
+// Remove Nth Node From End of List
+
+// Solution
+// Given the head of a linked list, remove the nth node from the end of the list and return its head.
+
+ 
+
+// Example 1:
+
+
+// Input: head = [1,2,3,4,5], n = 2
+// Output: [1,2,3,5]
+// Example 2:
+
+// Input: head = [1], n = 1
+// Output: []
+// Example 3:
+
+// Input: head = [1,2], n = 1
+// Output: [1]
+ 
+
+// Constraints:
+
+// The number of nodes in the list is sz.
+// 1 <= sz <= 30
+// 0 <= Node.val <= 100
+// 1 <= n <= sz
+ 
+
+// Follow up: Could you do this in one pass?
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+     
+//         vector<int> arr;
+        
+//         ListNode *temp = head;
+        
+//         while(temp)
+//         {
+//             arr.push_back(temp->val);
+//             temp = temp->next;
+//         }
+        
+//         ListNode *ans = new ListNode;
+//         ListNode *ans_head = ans;
+        
+//         for(int i = 0; i < arr.size(); i++)
+//             if (i != arr.size() - n)
+//             {
+//                 ans_head->next = new ListNode (arr[i]);
+//                 ans_head = ans_head->next;
+//             }
+
+//         return ans->next;
+        
+        
+        // Using Linked List will do later after learning linked list from cards
+        
+        // Now is the day today
+        
+        ListNode *slow = head, *fast = head;
+
+        for (int i = 0; i < n; i++) 
+            fast = fast->next;
+        
+        if (!fast) 
+            return head->next;
+        
+        while (fast->next) 
+            fast = fast->next, slow = slow->next;
+        
+        slow->next = slow->next->next;
+        
+        return head;
+        
+        
+    }
+};
